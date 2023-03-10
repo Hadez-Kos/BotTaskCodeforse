a17951dfc1c3b8f5b223bd007549991b480cd825
Andrey <andreykosov21@gmail.com>
2023-03-11 00:08:55 +0300

    Add models database

---
+++ task/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, MetaData, Table, ForeignKey
+
+metadata = MetaData()
+
+theme = Table(
+    "theme",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False)
+)
+
+task = Table(
+    "task",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("solution", Integer, nullable=False),
+    Column("name", String, nullable=False),
+    Column("number", Integer, nullable=False),
+    Column("difficulty", Integer, nullable=False)
+)
+
+intersection = Table(
+    "intersection",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("task_id", Integer, ForeignKey("task.id")),
+    Column("theme_id", Integer, ForeignKey("theme.id"))
+)
