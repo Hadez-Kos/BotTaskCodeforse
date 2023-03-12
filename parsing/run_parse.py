20dbffc2830a933f4c311e024e4d8185568ea3e1
Andrey <andreykosov21@gmail.com>
2023-03-12 19:14:19 +0300

    Add data in database, function parsing solved.

--- parsing/run_parse.py
+++ parsing/run_parse.py
@@ -5,33 +5,47 @@ import asyncio
 from task.database import Database
 
 
-async def get_offer_page(table_list: list):
+async def get_offer_page(table_list: list):  # Осуществляем сбор данных по каждой задаче в таблице
     list_task = list()
-    list_theme = list()
-    for element in table_list:
-        for tag in element.find_all('tr'):
-            td = list(tag.findAll('td'))
-            if td:
-                name, theme = td[1].findAll('div')
-                dct_task = {
-                    'solution': int(td[4].find('a').text.strip()[1:]),
-                    'name': name.find('a').text.strip(),
-                    'number': td[0].find('a').text.strip(),
-                    'difficulty': int(td[3].find('span').text.strip()),
-                    'url': td[0].find('a').get('href')
-                }
+    try:
+        for element in table_list:
+            for tag in element.find_all('tr'):
+                td = list(tag.findAll('td'))
+                if td:
+                    name, theme = td[1].findAll('div')
+                    dct_task = {
+                        'solution': int(td[4].find('a').text.strip()[1:]),
+                        'name': name.find('a').text.strip(),
+                        'number': td[0].find('a').text.strip(),
+                        'difficulty': int(td[3].find('span').text.strip()),
+                        'url': td[0].find('a').get('href'),
+                        'theme': [i.text.strip().lower() for i in theme.findAll('a')]
+                    }
 
-                dct_theme = {
-                    'name': [i.text.strip() for i in theme.findAll('a')]
-                }
-                list_task.append(dct_task)
-                list_theme.append(dct_theme)
-    return list_task, list_theme
+                    list_task.append(dct_task)
+    except:
+        return None
+    return list_task
+
+
+async def convert_data_database(data_json: list):
+    themes = set()
+    for i in data_json:
+        await Database().insert_data_task({'solution': i['solution'], 'name': i['name'], 'number': i['number'],
+                                               'difficulty': i['difficulty'], 'url': i['url']})
+
+        themes = themes | set(i['theme'])
+
+    await Database().insert_data_theme(themes)
+    for i in data_json:
+        await Database().insert_data_intersection(i)
 
 
 async def get_url(url: str):
     # читаем всем таблицы одной страницы
-    response = req.get(url)
+    response = req.get(url, params={'q': 'goog'}, headers={
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36',
+        'Accept-Language': 'ru'})
     soup = BS(response.text, 'lxml')
 
     try:
@@ -46,9 +60,10 @@ async def main():
     # зменение url страницы для чтения всех страниц
     number_page = 1
     component_url = URL_SITE.split('?')
+    tasks = []
 
     # читаем таблицу данных с каждой страницы, пока цикл не закончиться
-    while True:
+    while number_page < 2:
         url_site = component_url[0] + '/page/' + str(number_page) + f'?{component_url[1]}'
 
         try:
@@ -56,15 +71,14 @@ async def main():
         except:
             break
 
-        tasks, themes = await get_offer_page(table_tasks)
-
-        await Database().insert_data_task(tasks)
-        await Database().insert_data_theme(themes)
-
         if table_tasks:
-            print(table_tasks)
+            tasks += await get_offer_page(table_tasks)
+        else:
+            break
 
         number_page += 1
+
+    await convert_data_database(tasks)
 
 
 if __name__ == '__main__':
--- task/database.py
+++ task/database.py
@@ -1,21 +1,37 @@
 from sqlalchemy import create_engine
+from sqlalchemy.sql import select
 from .models import theme, task, intersection
 from config import DB_PASS, DB_NAME, DB_USER, DB_PORT, DB_HOST
+from sqlalchemy.orm import sessionmaker
 
 
 class Database:
     def __init__(self):
         self.engine = create_engine(f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}', echo=True)
+        self.Session = sessionmaker()
+        self.Session.configure(bind=self.engine)
+        self.session = self.Session()
 
     async def insert_data_task(self, data):
-        with self.engine.connect() as conn:
-            query_task = task.insert().values(data).returning(task)
-
-            dt = conn.execute(query_task).fetchall()
+        self.session.execute(task.insert().values(data))
+        self.session.commit()
 
     async def insert_data_theme(self, data):
-        with self.engine.connect() as conn:
-            query_theme = theme.insert().values(data).returning(theme)
+        for el in list(data):
+            self.session.execute(theme.insert().values(name=str(el)))
+        self.session.commit()
 
-            dt = conn.execute(query_theme).fetchall()
+    async def insert_data_intersection(self, data):
+        task_id = select(task).where(task.c.name == data['name'] and task.c.number == data['number'])
+        theme_id = []
+
+        for i in data['theme']:
+            theme_id.append(select(theme).where(theme.c.name == i))
+
+        for j in theme_id:
+            tk_id = self.session.execute(task_id).first()
+            te_id = self.session.execute(j).first()
+            self.session.execute(intersection.insert().values(task_id=tk_id[0], theme_id=te_id[0]))
+
+        self.session.commit()
 
